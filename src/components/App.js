ce9c76d23ae70394eeb802c331828f02a5cf1434
Rohit-del-source <rohitverma18158920@gmail.com>
2023-12-17 11:26:22 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
 const App = () => {
-  
+  const [img, setImg] = useState('')
+  const [loading, setLoading] = useState('')
+  const [caption, setcaption] = useState(false);
+  const [id, setId] = useState('');
+
+  useEffect(()=>{
+    if(id!=='') {
+      setLoading(true);
+      fetch('https://jsonplaceholder.typicode.com/photos/${id}')
+      .then(r=>r.json())
+      .then((r)=>{
+        setcaption(r.title);
+        setImg(r.url);
+        setLoading(false);
+      })
+    }
+  },[id])
+
+  return (
+    <div>
+     <label for="idNumber">Id number</label> 
+    <input id="idNumber" value={id} onChange={e=>setId(e.target.value)} />
+      {
+        loading && <Loader/>
+      }
+      {
+        img !== '' &&
+          <PhotoFrame src={img} caption={caption}/>
+      }
+    </div>
+  )
 }
 
 
--- src/components/PhotoFrame.js
+++ src/components/PhotoFrame.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const PhotoFrame = () => {
-   
+export const PhotoFrame = ({src, caption}) => {
+   return (
+      <div className="photoframe">
+         <img src={src} alt="img"/>
+         <div className="caption">{caption}</div>
+      </div>
+   )
 }
